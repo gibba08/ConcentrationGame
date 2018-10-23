af92fe34d845c32877192bf3316792bf760c4280
Gilberto Lo Re <gibba.lore@gmail.com>
2018-10-23 22:52:10 +0200

    first button with flip func created

--- ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	@IBAction func touchCard(_ sender: UIButton)
+	{
+		flipCard(withEmoji: "👻", on:sender)
 	}
-
-
+	
+	func flipCard(withEmoji emoji: String, on button: UIButton)
+	{
+		if button.currentTitle == emoji
+		{
+			button.setTitle("", for: UIControl.State.normal)
+			button.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+		}
+		else
+		{
+			button.setTitle(emoji, for: UIControl.State.normal)
+			button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+		}
+	}
+	
+	
 }
 
